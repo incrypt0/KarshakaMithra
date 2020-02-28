78ed33801732e17a0b1ed4162c6e614d7eaa8df2
krishna-incrypto <vpkrishnanand@gmail.com>
2020-02-28 16:37:58 +0530

    UI improved

---
+++ lib/data/scheme.dart
@@ -0,0 +1,17 @@
+class Scheme {
+  final String name;
+  final String imgUrl;
+  final String description;
+  final String link;
+
+  Scheme({this.name, this.description, this.imgUrl, this.link});
+}
+
+List<Scheme> schemeList = [
+  Scheme(
+    name: "PMKY",
+    description: "blahaflsdklnkangnkdfnkgnklfadnklgnklnekasdnkglsklglkmlkm",
+    imgUrl: "http:/blah.com/gfhf.jpg",
+    link: "www.facebook.com",
+  ),
+];
